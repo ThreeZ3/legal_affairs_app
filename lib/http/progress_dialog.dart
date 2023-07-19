6301d4e619d192dd78bef2e3837207c9c489a255
Three.z <56373480+ThreeZ3@users.noreply.github.com>
2023-07-19 17:20:27 +0800

    Update progress_dialog.dart

--- lib/http/progress_dialog.dart
+++ lib/http/progress_dialog.dart
@@ -2,11 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-/// 创建者：王增阳
-/// 开发者：王增阳
-/// 版本：1.0
-/// 创建日期：2020-02-19
-///
+
 _ProgressOverlayState _state;
 
 class ProgressOverlay extends StatefulWidget {
