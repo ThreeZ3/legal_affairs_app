1283c48644ff41acf512a9479566d2393ce1c080
Three.z <56373480+ThreeZ3@users.noreply.github.com>
2023-07-19 17:20:03 +0800

    Update base_view_model.dart

--- lib/http/base_view_model.dart
+++ lib/http/base_view_model.dart
@@ -1,11 +1,7 @@
 import 'dart:async';
 export 'base_respone_model.dart';
 
-/// 创建者：王增阳
-/// 开发者：王增阳
-/// 版本：1.0
-/// 创建日期：2020-02-19
-///
+
 typedef Response(int code,String msg);
 
 class BaseViewModel {
